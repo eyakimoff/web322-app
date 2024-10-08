9e0e192d1b0f06cc7bc6d0605b13356fd9568549
Eric Yakimoff <eyakimoff@myseneca.ca>
2024-10-08 19:15:53 -0400

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*********************************************************************************
 WEB322 – Assignment 02
-I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.
 No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 TODO:
 Name: 
@@ -10,3 +10,9 @@ Vercel Web App URL:
 GitHub Repository URL: 
 
 ********************************************************************************/
+const express = require("express"); // "require" the Express module
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+
+// start the server on the port and output a confirmation to the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
