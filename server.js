6472b383db5bad1ea0036cfd3b3b3ca70f315b89
Eric Yakimoff <eyakimoff@myseneca.ca>
2024-10-08 17:09:50 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+/*********************************************************************************
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+TODO:
+Name: 
+Student ID: 
+Date: 
+Vercel Web App URL: 
+GitHub Repository URL: 
+
+********************************************************************************/
